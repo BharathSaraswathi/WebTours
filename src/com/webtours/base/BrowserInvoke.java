e2ea978449757e9b7f7dcbec718b756d5d77c933
Bharath <saraswathibharath@gmail.com>
2020-03-30 09:22:33 +0530

    no message

---
+++ src/com/webtours/base/BrowserInvoke.java
@@ -0,0 +1,19 @@
+package com.webtours.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserInvoke {
+
+	public static WebDriver driver = null;
+
+	public static void main(String[] args) {
+		//Creating Driver Object for Chrome
+		driver = new ChromeDriver();
+		//Invokig the site on Chrome Browser
+		driver.get("http://newtours.demoaut.com/");
+		driver.close();
+
+	}
+
+}
