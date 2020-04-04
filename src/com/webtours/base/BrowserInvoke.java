9332e27151a82d1fbabb0e83b8be2b854e19d4b7
Bharath <saraswathibharath@gmail.com>
2020-04-04 14:15:50 +0530

    New Changes

--- src/com/webtours/base/BrowserInvoke.java
+++ src/com/webtours/base/BrowserInvoke.java
@@ -1,17 +1,32 @@
 package com.webtours.base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class BrowserInvoke {
 
 	public static WebDriver driver = null;
 
 	public static void main(String[] args) {
-		//Creating Driver Object for Chrome
+		// Creating Driver Object for Chrome
 		driver = new ChromeDriver();
-		//Invoking the site on Chrome Browser
+		// Invoking the site on Chrome Browser
 		driver.get("http://newtours.demoaut.com/");
+		// Identify Register link
+		WebElement registerLink = driver.findElement(By.xpath(".//a[contains(text(),'REGISTER')]"));
+		// Click on Register link
+		registerLink.click();
+		// Enter Firstnames
+		WebElement firstName = driver.findElement(By.xpath(".//input[@name='firstName']"));
+		firstName.sendKeys("SELENIUM");
+		// Enter SecondName
+		WebElement secondName = driver.findElement(By.xpath(".//input[@name='secondName']"));
+		secondName.sendKeys("JAVA");
+		Select countryName = new Select(driver.findElement(By.xpath(".//select[@name='country']")));
+		countryName.selectByValue("92");
 		driver.close();
 
 	}
